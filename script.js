bbe9ef25c036ed0e98a56121fd9051b61da22bdb
ACHRAF HEND KIAS <achrafhendkias40@gmail.com>
2026-09-20 14:03:16 +0100

    initilized gallery

--- script.js
+++ script.js
@@ -11,10 +11,21 @@ const closeNavMenu= () => {
     navMenu.style.left = "-300px";
 }
 
-const go = () =>{
-    // slider.style.animationN
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
 
-}
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
 
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
 
 
